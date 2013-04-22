324f1a37422b83b4e4f2c98d295603f0a0e37343
wcdolphin <wcdolphin@gmail.com>
2013-04-22 14:17:41 -0400

    update to redirect for Facebook Oauth

--- boilerflask/views.py
+++ boilerflask/views.py
@@ -1,22 +1,44 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for
 #from boilerflask.models import User
 #from boilerflask.forms import (LoginForm)
 from boilerflask import app #, loginManager, crypt, db, cache
 import utils
-import facebook
+from flask_oauth import OAuth
+
+
+oauth = OAuth()
+
+facebook = oauth.remote_app('facebook',
+    base_url='https://graph.facebook.com/',
+    request_token_url=None,
+    access_token_url='/oauth/access_token',
+    authorize_url='https://www.facebook.com/dialog/oauth',
+    consumer_key=app.config['FACEBOOK_APP_ID'],
+    consumer_secret=app.config['FACEBOOK_APP_SECRET'],
+    request_token_params={'scope': 'email'}
+)
 
 @app.route('/', methods=['GET'] )
 def index():
-    user = getFacebookUser()
-    facebook_profile = None
-    notifications = None
-    if user:
+    if not 'oauth_token' in session:
+        return redirect(url_for('login'))
+    else:
 
-        graph = facebook.GraphAPI(user["access_token"])
-        facebook_profile = graph.get_object("me")
-        notifications = graph.get_object("me/notifications")
+        facebook_profile = facebook.get('/me')
 
-    print "User:%s" % user
+        if facebook_profile.status == 200: #200 means success
+            facebook_profile =  facebook_profile.data
+        else:
+            print "get facebook/me failed"
+
+
+        notifications = facebook.get("me/notifications")
+        
+        if notifications.status == 200: #200 means success
+            notifications =  notifications.data
+        else:
+            print "get facebook/me/notifications failed"
+
     print "Profile:%s" % facebook_profile
     print "Notifications:%s" % notifications
 
@@ -24,25 +46,42 @@ def index():
 
 @app.route('/notifier', methods=['GET'] )
 def notifier():
-    user = getFacebookUser()
-    facebook_profile = None
-    notifications = None
-    # check notifications
-    # decide to send notifications
-    # blahahaha
-    if user:
-        graph = facebook.GraphAPI(user["access_token"])
-        facebook_profile = graph.get_object("me")
-        notifications = graph.get_object("me/notifications")
+    if not 'oauth_token' in session:
+        return redirect(url_for('login'))
+    notifications = facebook.get("me/notifications")
+    
+    if notifications.status == 200: #200 means success
+        notifications =  notifications.data
+    else:
+        print "get facebook/me/notifications failed"
 
-    print "User:%s" % user
-    print "Profile:%s" % facebook_profile
-    print "Notifications:%s" % notifications
-    return "foo"
+    return '%s' % notifications
 
-def getFacebookUser():
-    user = facebook.get_user_from_cookie(request.cookies, app.config['FACEBOOK_APP_ID'], app.config['FACEBOOK_APP_SECRET'])
-    return user
+@app.route('/login')
+def login():
+    return facebook.authorize(callback=url_for('facebook_authorized',
+        next=request.args.get('next') or request.referrer or None,
+        _external=True))
+
+
+@app.route('/login/authorized')
+@facebook.authorized_handler
+def facebook_authorized(resp): #you can/should do registration here
+    if resp is None:
+        return 'Access denied: reason=%s error=%s' % (
+            request.args['error_reason'],
+            request.args['error_description']
+        )
+    session['oauth_token'] = (resp['access_token'], '')
+
+    return redirect(url_for('index')) # redirect to home/index page
+
+
+@facebook.tokengetter
+def get_facebook_oauth_token():
+    return session.get('oauth_token')
+
+
 
 @app.errorhandler(404)
 def page_not_found(error):
